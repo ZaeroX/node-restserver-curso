b2af6b5075682a4ed50ecaa7b7be26d0e3c47d58
Victor <ferruda@gmail.com>
2019-11-26 22:53:19 -0400

    restserver- usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,19 @@
 //=================================
 
 process.env.PORT = process.env.PORT || 3000;
+//==========================
+//=================entorno==
+//=========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:xERjCxlASXEP03ZA@cluster0-fknq4.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -19,14 +19,15 @@ app.use(require('./routes/usuario'));
     console.log('Base de datos ONLINE');
 })*/
 
-mongoose.connect('mongodb://localhost:27017/cafe', {
+mongoose.connect(process.env.URLDB, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-    /*, (err, res) => {
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    },
+    (err, res) => {
         if (err) throw err;
         console.log('entor a la base');
-    }*/
+    }
 );
 
 app.listen(process.env.PORT, () => {
